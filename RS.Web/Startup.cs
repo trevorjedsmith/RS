fcb8473a08d6953ef4149f233f7b79bd035585aa
trevor <trevor_jed_smith@hotmail.com>
2017-07-10 19:30:00 +0930

    Initial Commit

---
+++ RS.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RS.Web.Startup))]
+namespace RS.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
